92fff8d42224caee3f0e0cc52c7549dd452352a0
atharkundark <atharkun@gmail.com>
2025-04-17 02:56:23 +0700

    tambahan

--- js/script.js
+++ js/script.js
@@ -29,3 +29,26 @@ function setSenderUI(name, birthDate, gender, messages) {
     document.getElementById("sender-gender").innerHTML = gender;
     document.getElementById("sender-messages").innerHTML = messages;
 }
+
+// untuk banner slide
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n){
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("bannerku");
+    if (n > x.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = x.length};
+    for (i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+setInterval(() => {
+    plusDivs(1);
+},2000)

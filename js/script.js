a80cbb437aaf0dd228221cdf76af13276500c967
atharkundark <atharkun@gmail.com>
2025-04-17 02:10:12 +0700

    diperbarui

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-let UserName = prompt("siapakah nama anda", " ");
-document.getElementById("user-name").innerHTML = UserName
+// let nama = prompt("what is your name?", " ");
+// document.getElementById("user-wlc").innerHTML = nama;
 
-
-function validateform(){
-    const name = document.forms["message-form"]["namaku"].value;
-    const birthDate = document.forms["message-form"]["Tanggalku"].value;
-    const gender = document.forms["message-form"]["gender"].value;
-    const messages = document.forms["message-form"]["messages"].value;
-
+// validasi form
+function ValidateForm(){
+    const name = document.forms["contact-form"]["namaku"].value;
+    const birthDate = document.forms["contact-form"]["tanggalku"].value;
+    const gender = document.forms["contact-form"]["gender"].value;
+    const messages = document.forms["contact-form"]["messages"].value;
+// error jika kosong
     if (name == " " || birthDate == " " || gender == " " || messages == " "){
         alert("tidak boleh kosong");
         return false;
@@ -17,8 +17,13 @@ function validateform(){
 
     return false;
 }
-
+// mengirimkan ke senderUI
 function setSenderUI(name, birthDate, gender, messages) {
+
+    // menampilkan waktu real
+    var dt = new Date ();
+    document.getElementById("tanggalwaktu").innerHTML = dt.toLocaleString();
+    //menampilkan apa yang kita isi di form
     document.getElementById("sender-full-name").innerHTML = name;
     document.getElementById("sender-birth-date").innerHTML = birthDate;
     document.getElementById("sender-gender").innerHTML = gender;
